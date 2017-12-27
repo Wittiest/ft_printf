3fd74975a2092103d3937b0f339a9cc1fed7e234
Wittiest <dustinparkerpearson@gmail.com>
2017-12-26 19:39:43 -0800

    p char prefix

--- hex.c
+++ hex.c
@@ -40,8 +40,8 @@ static int		print_hex_upp(uintmax_t	u_arg)
 
 int		print_hex(t_start *start)
 {
-	if (start->c == 'x' || start->c == 'p')
-		return(print_hex_low(start->u_arg));
-	else
+	if (start->c == 'X')
 		return(print_hex_upp(start->u_arg));
+	else
+		return(print_hex_low(start->u_arg, start->c == 'p'));
 }
